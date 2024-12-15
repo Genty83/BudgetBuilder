21e59fd468adea1e63349c61a5e671a0b016e14a
CraigGent <genty83@outlook.com>
2024-12-15 21:04:06 +0000

    Various changes to finance app

--- backend/finance/models.py
+++ backend/finance/models.py
@@ -1,31 +1,15 @@
 from django.db import models
 
 class Income(models.Model):
-    """
-    Represents an income entry with details such as name, amount,
-    date received, the person who received it, the month, and the year.
-    
-    Attributes:
-        name (str): The name or source of the income.
-        amount (Decimal): The amount of income received.
-        date_received (Date): The date the income was received.
-        received_by (str): The person who received the income.
-        month (str): The month when the income was received.
-        year (int): The year when the income was received.
-    """
-
-    name = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date_received = models.DateField()
-    received_by = models.CharField(max_length=100)
-    month = models.CharField(max_length=20)
-    year = models.IntegerField()
+    name = models.CharField(max_length=100, blank=True, null=True)        
+    amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  
+    date_received = models.DateField(blank=True, null=True)              
+    received_by = models.CharField(max_length=100, blank=True, null=True) 
+    month = models.CharField(max_length=20, blank=True, null=True)  # Change month to CharField
+    year = models.IntegerField(blank=True, null=True)                    
 
     def __str__(self):
-        """
-        Returns a string representation of the Income instance.
+        return self.name if self.name else 'Income Entry'
 
-        Returns:
-            str: A string describing the income details.
-        """
-        return f"{self.name} - {self.amount} received on {self.date_received}"
+    class Meta:
+        ordering = ['id']  # Order by 'id'
--- backend/finance/urls.py
+++ backend/finance/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import FinanceManagerView
+from .views import IncomeListView, FinanceManagerView
 
 urlpatterns = [
-    path('finance-manager/', FinanceManagerView.as_view(), name='finance-manager'),
+    path('', FinanceManagerView.as_view(), name='finance-manager'),
+    path('api/incomes/', IncomeListView.as_view(), name='income-list'),
+    path('api/incomes/<int:pk>/', IncomeListView.as_view(), name='income-detail'),  # URL pattern for delete and detail
 ]
